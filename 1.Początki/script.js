54f542aca9ca5d8f1c2e044683fdcfec3233c1cd
Damian Pedrycz <damian.pedrycz@onet.eu>
2020-11-08 11:31:08 +0100

    First chapter completed

---
+++ 1.Początki/script.js
@@ -0,0 +1,12 @@
+//Zadanie 1:
+console.log("Damian Pedrycz");
+
+//Zadanie 2:
+console.log(12 + 32);
+console.log(52 / 16);
+console.log(5 * 2);
+
+// Zadanie 3:
+console.log("Hello World");
+console.log(Hello World);
+console.log("Hello World")
