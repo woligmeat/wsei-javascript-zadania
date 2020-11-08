2722ec61b72846558c43bdfd6a358af90c708226
Damian Pedrycz <damian.pedrycz@onet.eu>
2020-11-08 11:39:14 +0100

    Third chapter completed

---
+++ 3.Operatory/script.js
@@ -0,0 +1,48 @@
+//Zadanie 1.
+let isEmpty = false;
+let isGold = true;
+
+console.log(isEmpty == isGold);
+
+//Zadanie 2.
+let firstVariable = 4;
+let secondVariable = 3;
+let moduleResult = 0;
+
+moduleResult = firstVariable % secondVariable;
+console.log(moduleResult);
+
+//Zadanie 3
+let someNumber = '4';
+let someString = '425';
+let stringResult = "";
+
+stringResult = someString + someNumber;
+console.log(stringResult);
+
+
+
+
+//Zadanie 4.
+var someNumber = 425;
+var someString = "425";
+console.log(someNumber == someString);
+console.log(someNumber === someString);
+// w pierwszym przykladzie jest true poniewaz są te same liczby
+// w drugim przykladzie jest false poniewaz są rózne typy zmiennych
+
+
+//Zadanie 5
+let counter = 30;
+console.log(counter);
+counter++;
+console.log(counter);
+counter--;
+console.log(counter);
+
+//Zadanie 6
+let variableOne = 30;
+let variableTwo = 20;
+let result = null;
+result = variableOne > variableTwo;
+console.log(result);
